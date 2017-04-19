772956e4db953b913199844b5494578b5aba3bac
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-04-19 11:06:57 +0200

    EMP-9391 Added basic project structure

---
+++ Exposure/Exposure.h
@@ -0,0 +1,19 @@
+//
+//  Exposure.h
+//  Exposure
+//
+//  Created by Fredrik Sjöberg on 2017-04-19.
+//  Copyright © 2017 emp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Exposure.
+FOUNDATION_EXPORT double ExposureVersionNumber;
+
+//! Project version string for Exposure.
+FOUNDATION_EXPORT const unsigned char ExposureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Exposure/PublicHeader.h>
+
+
