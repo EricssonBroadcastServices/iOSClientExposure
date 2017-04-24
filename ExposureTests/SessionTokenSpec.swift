d62a9118f3c25785d695859335122d350f6dcf14
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-04-24 10:13:10 +0200

    EMP-9392 Added unit tests for several paths

---
+++ ExposureTests/SessionTokenSpec.swift
@@ -0,0 +1,33 @@
+//
+//  SessionTokenSpec.swift
+//  Exposure
+//
+//  Created by Fredrik Sjöberg on 2017-04-24.
+//  Copyright © 2017 emp. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import Exposure
+
+
+class SessionTokenSpec: QuickSpec {
+    override func spec() {
+        super.spec()
+        
+        describe("AuthorizationHeaders") {
+            it("should produce authorization headers") {
+                let token = SessionToken(value: "TOKEN")
+                let header = token?.authorizationHeader
+                
+                expect(header).toNot(beNil())
+                
+                let value:String? = header!["Authorization"]
+                
+                expect(value).toNot(beNil())
+                expect(value!).to(equal("Bearer "+"TOKEN"))
+            }
+        }
+    }
+}
