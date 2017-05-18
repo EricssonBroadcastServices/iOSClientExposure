6cc00a203b66cf1ada7522f490ce9faf8ca6c83f
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-05-18 15:27:56 +0200

    EMP-9596 AssetList properties added

--- Exposure/Asset/Response/AssetList.swift
+++ Exposure/Asset/Response/AssetList.swift
@@ -10,9 +10,26 @@ import Foundation
 import SwiftyJSON
 
 public struct AssetList: ExposureConvertible {
+    public let totalCount: Int?
+    public let pageSize: Int?
+    public let pageNumber: Int?
+    public let items: [Asset]?
     
     public init?(json: Any) {
         let actualJson = JSON(json)
         
+        totalCount = actualJson[JSONKeys.totalCount.rawValue].int
+        pageSize = actualJson[JSONKeys.pageSize.rawValue].int
+        pageNumber = actualJson[JSONKeys.pageNumber.rawValue].int
+        items = actualJson[JSONKeys.items.rawValue].arrayObject?.flatMap{ Asset(json: $0) }
+        
+        if totalCount == nil && pageSize == nil && pageNumber == nil && items == nil { return nil }
+    }
+    
+    internal enum JSONKeys: String {
+        case totalCount = "totalCount"
+        case pageSize = "pageSize"
+        case pageNumber = "pageNumber"
+        case items = "items"
     }
 }
