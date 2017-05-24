6109d0d0d1409fb3e9ed24653b5a9553e6404470
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-05-24 21:19:26 +0200

    EMP-9596 Unit tests for AssetList

--- ExposureTests/Asset/AssetListSpec.swift
+++ ExposureTests/Asset/AssetListSpec.swift
@@ -19,8 +19,57 @@ class AssetListSpec: QuickSpec {
         
         describe("JSON") {
             it("should succeed with valid response") {
+                let value = AssetList(json: AssetListJSON.valid())
                 
+                expect(value).toNot(beNil())
+                expect(value!.totalCount).toNot(beNil())
+                expect(value!.pageSize).toNot(beNil())
+                expect(value!.pageNumber).toNot(beNil())
+                expect(value!.items).toNot(beNil())
             }
+            
+            it("should init with partial response") {
+                let value = AssetList(json: AssetListJSON.missingKeys())
+                
+                expect(value).toNot(beNil())
+                expect(value!.totalCount).toNot(beNil())
+                expect(value!.pageSize).to(beNil())
+                expect(value!.pageNumber).to(beNil())
+                expect(value!.items).to(beNil())
+            }
+            
+            it("should not init with empty or non matching response") {
+                let value = AssetList(json: AssetListJSON.empty())
+                
+                expect(value).to(beNil())
+            }
+        }
+    }
+}
+
+extension AssetListSpec {
+    enum AssetListJSON {
+        static let totalCount = 10
+        static let pageSize = 1
+        static let pageNumber = 1
+        static let items = [AssetSpec.AssetJSON.valid()]
+        static func valid() -> Any {
+            return [
+                "totalCount": AssetListJSON.totalCount,
+                "pageSize": AssetListJSON.pageSize,
+                "pageNumber": AssetListJSON.pageNumber,
+                "items": AssetListJSON.items
+            ]
+        }
+        
+        static func missingKeys() -> Any {
+            return [
+                "totalCount": AssetListJSON.totalCount
+            ]
+        }
+        
+        static func empty() -> Any {
+            return [:]
         }
     }
 }
