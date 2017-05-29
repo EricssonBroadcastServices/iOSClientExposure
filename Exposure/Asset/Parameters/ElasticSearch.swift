bac26c621c7844a2176ca7415271f4dbf6aedba3
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-05-29 14:54:38 +0200

    EMP-9596 Added elastic search

---
+++ Exposure/Asset/Parameters/ElasticSearch.swift
@@ -0,0 +1,24 @@
+//
+//  ElasticSearch.swift
+//  Exposure
+//
+//  Created by Fredrik Sjöberg on 2017-05-29.
+//  Copyright © 2017 emp. All rights reserved.
+//
+
+import Foundation
+
+/*public protocol ElasticSearch {
+    var elasticSearchFilter: ElasticSearchQuery { get set }
+}
+
+extension ElasticSearch {
+    /// https://www.elastic.co/guide/en/elasticsearch/reference/1.7/query-dsl-query-string-query.html
+    public func query(with string: String) -> Self {
+        
+    }
+}
+
+public struct ElasticSearchQuery {
+    public let query: String
+}*/
