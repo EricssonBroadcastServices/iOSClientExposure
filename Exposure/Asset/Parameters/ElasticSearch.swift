8d52639e2293be2d1bab09a473fe007efe11b373
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-05-29 21:27:59 +0200

    EMP-9688 Added elastic search query params

--- Exposure/Asset/Parameters/ElasticSearch.swift
+++ Exposure/Asset/Parameters/ElasticSearch.swift
@@ -8,17 +8,29 @@
 
 import Foundation
 
-/*public protocol ElasticSearch {
-    var elasticSearchFilter: ElasticSearchQuery { get set }
+public protocol ElasticSearch {
+    var elasticSearchQuery: ElasticSearchQuery { get set }
 }
 
 extension ElasticSearch {
     /// https://www.elastic.co/guide/en/elasticsearch/reference/1.7/query-dsl-query-string-query.html
-    public func query(with string: String) -> Self {
-        
+    public var elasticSearchString: String? {
+        return elasticSearchQuery.query
+    }
+    
+    /// Will replace any currently active query
+    /// https://www.elastic.co/guide/en/elasticsearch/reference/1.7/query-dsl-query-string-query.html
+    public func elasticSearch(query string: String?) -> Self {
+        var old = self
+        old.elasticSearchQuery = ElasticSearchQuery(query: string)
+        return old
     }
 }
 
 public struct ElasticSearchQuery {
-    public let query: String
-}*/
+    public let query: String?
+    
+    internal init(query: String? = nil) {
+        self.query = query
+    }
+}
