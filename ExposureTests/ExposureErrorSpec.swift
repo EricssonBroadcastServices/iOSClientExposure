23ad01cce457723bb3a837c740445650fd27857b
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-06-13 12:27:08 +0200

    EMP-9798 ExposureError unit tests

---
+++ ExposureTests/ExposureErrorSpec.swift
@@ -0,0 +1,37 @@
+//
+//  ExposureErrorSpec.swift
+//  Exposure
+//
+//  Created by Fredrik Sjöberg on 2017-06-13.
+//  Copyright © 2017 emp. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import Exposure
+
+class ExposureErrorSpec: QuickSpec {
+    override func spec() {
+        super.spec()
+        
+        describe("ExposureError") {
+            it("should describe object error") {
+                let reason = ExposureError.serialization(reason: ExposureError.SerializationFailureReason.objectSerialization(reason: "REASON", json: [:]))
+                
+                let desc = reason.localizedDescription
+                
+                expect(desc).to(equal("Object Serialization error: REASON for json: \([:])"))
+            }
+            
+            it("should describe jsonSerialization error") {
+                let anyError = NSError(domain: "domain", code: 1, userInfo: nil)
+                let reason = ExposureError.serialization(reason: ExposureError.SerializationFailureReason.jsonSerialization(error: anyError))
+                
+                let desc = reason.localizedDescription
+                
+                expect(desc).to(equal("JSON Serialization error: \(anyError.localizedDescription)"))
+            }
+        }
+    }
+}
