f84388b5856d3234b24c1939d877880b70303774
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-06-14 16:52:03 +0200

    EMP-9698 Protocol conformance

--- Exposure/EPG/Request/FetchEpgList.swift
+++ Exposure/EPG/Request/FetchEpgList.swift
@@ -8,10 +8,64 @@
 
 import Foundation
 
-public struct FetchEpgList {
+public struct FetchEpgList: Exposure, SortedResponse, PageableResponse, FilteredPublish {
+    public typealias Response = [ChannelEpg]
+    
+    public var endpointUrl: String {
+        return environment.apiUrl + "/content/asset"
+    }
+    
+    public var parameters: [String: Any] {
+        return queryParams
+    }
+    
+    public var headers: [String: String]? {
+        return nil
+    }
+    
+    
+    public var sortDescription: SortDescription
+    public var pageFilter: PageFilter
+    public var publishFilter: PublishFilter
+    
     public let environment: Environment
     
-    public init(environment: Environment) {
+    internal init(environment: Environment) {
         self.environment = environment
+        self.sortDescription = SortDescription()
+        self.pageFilter = PageFilter()
+        self.publishFilter = PublishFilter()
+    }
+    
+    internal enum Keys: String {
+        case onlyPublished = "onlyPublished"
+        case pageSize = "pageSize"
+        case pageNumber = "pageNumber"
+        case sort = "sort"
+    }
+    
+    internal var queryParams: [String: Any] {
+        var params:[String: Any] = [
+            Keys.onlyPublished.rawValue: publishFilter.onlyPublished,
+            Keys.pageNumber.rawValue: pageFilter.page,
+            Keys.pageSize.rawValue: pageFilter.size
+        ]
+        
+        if let sort = sortDescription.descriptors {
+            // Query string is keys separated by ",".
+            // Any descending key should include a "-" sign as a prefix.
+            params[Keys.sort.rawValue] = sort
+                .map{ $0.ascending ? "" : "-" + $0.key }
+                .joined(separator: ",")
+        }
+        
+        return params
+    }
+}
+
+// MARK: - Request
+extension FetchEpgList {
+    public func request() -> ExposureRequest {
+        return request(.get, encoding: ExposureURLEncoding.default)
     }
 }
