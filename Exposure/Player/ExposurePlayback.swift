f7ba491780d0ce7935ba3f3cce960be5d09b2da4
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-07-11 13:04:54 +0200

    EMP-9801 Revert specialized streaming methods.

--- Exposure/Player/ExposurePlayback.swift
+++ Exposure/Player/ExposurePlayback.swift
@@ -10,14 +10,12 @@ import Foundation
 import Player
 
 public protocol ExposurePlayback {
-    func stream(vod entitlement: PlaybackEntitlement) throws
-    func stream(catchup entitlement: PlaybackEntitlement) throws
-    func stream(live entitlement: PlaybackEntitlement) throws
+    func stream(playback entitlement: PlaybackEntitlement) throws
     func offline(playback entitlement: PlaybackEntitlement) throws
 }
 
 extension Player: ExposurePlayback {
-    public func stream(vod entitlement: PlaybackEntitlement) throws {
+    public func stream(playback entitlement: PlaybackEntitlement) throws {
         guard let mediaLocator = entitlement.mediaLocator else {
             throw PlayerError.asset(reason: .missingMediaUrl)
         }
@@ -26,27 +24,7 @@ extension Player: ExposurePlayback {
         
         stream(url: mediaLocator, using: requester)
     }
-    
-    public func stream(catchup entitlement: PlaybackEntitlement) throws {
-        guard let mediaLocator = entitlement.mediaLocator else {
-            throw PlayerError.asset(reason: .missingMediaUrl)
-        }
         
-        let requester = ExposureFairplayRequester(entitlement: entitlement)
-        
-        stream(url: mediaLocator, using: requester)
-    }
-    
-    public func stream(live entitlement: PlaybackEntitlement) throws {
-        guard let mediaLocator = entitlement.mediaLocator else {
-            throw PlayerError.asset(reason: .missingMediaUrl)
-        }
-        
-        let requester = ExposureFairplayRequester(entitlement: entitlement)
-        
-        stream(url: mediaLocator, using: requester)
-    }
-    
     public func offline(playback entitlement: PlaybackEntitlement) throws {
         
     }
