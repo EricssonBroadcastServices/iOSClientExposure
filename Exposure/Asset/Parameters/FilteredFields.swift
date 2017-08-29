2efe47a13764e2c748642561972c82e14f1de99d
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-08-29 14:08:13 +0200

    EMP-10209 FieldsFilter docs

--- Exposure/Asset/Parameters/FilteredFields.swift
+++ Exposure/Asset/Parameters/FilteredFields.swift
@@ -8,37 +8,53 @@
 
 import Foundation
 
+/// Defines an *Exposure request* where the response fields may be selectively filtered.
 public protocol FilteredFields {
     typealias FieldSet = FieldsFilter.FieldSet
+    
+    /// Filter to apply
     var fieldsFilter: FieldsFilter { get set }
 }
 
 extension FilteredFields {
+    /// Retruns the `FieldSet` specified by the request.
     public var fieldSet: FieldSet {
         return fieldsFilter.fieldSet
     }
     
+    /// Specify the `FieldSet` wanted in the response.
+    ///
+    /// FieldSets are designed to allow granularity in response data, for example by avoiding large amounts unwanted data. Using any of these *pre-sets* will include a baseline of fields in the response.
     public func use(fieldSet: FieldSet) -> Self {
         var old = self
         old.fieldsFilter = FieldsFilter(with: self.fieldsFilter, fieldSet: fieldSet)
         return old
     }
     
-    
+    /// Return the fields specifically specified to be included. Optional
     public var fieldsIncluded: [String]? {
         return fieldsFilter.includedFields
     }
-    // The set of fields to include by default.
+    
+    /// Specify the set of fields to include in the response.
+    ///
+    /// - parameter fields: the fields to include
+    /// - returns: `Self`
     public func include(fields: [String]?) -> Self {
         var old = self
         old.fieldsFilter = FieldsFilter(with: self.fieldsFilter, includedFields: fields)
         return old
     }
     
+    /// Return the fields specifically specified to be excluded. Optional
     public var fieldsExcluded: [String]? {
         return fieldsFilter.excludedFields
     }
-    // List of fields to remove from the response.
+    
+    /// Specify the set of fields to exclude in the response.
+    ///
+    /// - parameter fields: the fields to exclude
+    /// - returns: `Self`
     public func exclude(fields: [String]?) -> Self {
         var old = self
         old.fieldsFilter = FieldsFilter(with: self.fieldsFilter, excludedFields: fields)
@@ -46,15 +62,27 @@ extension FilteredFields {
     }
 }
 
+/// Used internally to configure the filter
 public struct FieldsFilter {
+    /// Baseline `FieldSet` to include
     public enum FieldSet: String {
+        /// No fields will be included by default
         case empty = "NONE"
+        
+        /// A slimmed down set of fields will be included by default
         case partial = "PARTIAL"
+        
+        /// All fields are included by default
         case all = "ALL"
     }
     
+    /// The baseline field set
     internal let fieldSet: FieldSet
+    
+    /// Filtered fields to include
     internal let includedFields: [String]?
+    
+    /// Filtered fields to exclude
     internal let excludedFields: [String]?
     
     internal init(fieldSet: FieldSet = .all, includedFields: [String]? = nil, excludedFields: [String]? = nil) {
