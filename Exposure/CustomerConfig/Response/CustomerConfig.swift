1eeac1d9f3b3f8624c3e4a3cd2683c7dc428087d
Viktor Gardart <viktor@gardart.se>
2017-10-03 16:22:29 +0200

    🔥 added customer, BU, filename and config params

--- Exposure/CustomerConfig/Response/CustomerConfig.swift
+++ Exposure/CustomerConfig/Response/CustomerConfig.swift
@@ -14,11 +14,24 @@ public struct CustomerConfig: Codable {
   public let fileNames: [String]
   
   public struct File: Decodable {
-    public let data: [String: AnyJSONType]
+    public let customer: String
+    public let businessUnit: String
+    public let fileName: String
+    public let config: [String: AnyJSONType]
     
     public init(from decoder: Decoder) throws {
-      let container = try decoder.singleValueContainer()
-      data = try container.decode([String: AnyJSONType].self)
+      let container = try decoder.container(keyedBy: CodingKeys.self)
+      customer = try container.decode(String.self, forKey: .customer)
+      businessUnit = try container.decode(String.self, forKey: .businessUnit)
+      fileName = try container.decode(String.self, forKey: .fileName)
+      config = try container.decode([String: AnyJSONType].self, forKey: .config)
+    }
+
+    internal enum CodingKeys: String, CodingKey {
+      case customer
+      case businessUnit
+      case fileName
+      case config
     }
   }
 }
