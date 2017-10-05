b7e509faa7fcdc01320dd73756f20187d478f8e7
Viktor Gardart <viktor@gardart.se>
2017-10-05 10:52:44 +0200

    🌹 Tests for CarouselItem added

--- ExposureTests/Carousel/FetchCarouselListSpec.swift
+++ ExposureTests/Carousel/FetchCarouselListSpec.swift
@@ -15,32 +15,32 @@ import Mockingjay
 
 class FetchCarouselListSpec: QuickSpec {
 
-  override func spec() {
-    super.spec()
+    override func spec() {
+        super.spec()
 
-    let base = "https://exposure.empps.ebsd.ericsson.net"
-    let customer = "BlixtGroup"
-    let businessUnit = "Blixt"
-    let env = Environment(baseUrl: base, customer: customer, businessUnit: businessUnit)
+        let base = "https://exposure.empps.ebsd.ericsson.net"
+        let customer = "BlixtGroup"
+        let businessUnit = "Blixt"
+        let env = Environment(baseUrl: base, customer: customer, businessUnit: businessUnit)
 
-    let groupId = "abc123"
+        let groupId = "abc123"
 
-    let fetchList = FetchCarouselList(groupId: groupId,
-                                      environment: env)
+        let fetchList = FetchCarouselList(groupId: groupId,
+                                          environment: env)
 
-    describe("Basics") {
+        describe("Basics") {
 
-      it("should have no headers") {
-        expect(fetchList.headers).to(beNil())
-      }
+            it("should have no headers") {
+                expect(fetchList.headers).to(beNil())
+            }
 
-      it("should generate a correct endpoint url") {
-        let endpoint = "/carouselgroup/" + groupId
-        expect(fetchList.endpointUrl).to(equal(env.apiUrl + endpoint))
-      }
+            it("should generate a correct endpoint url") {
+                let endpoint = "/carouselgroup/" + groupId
+                expect(fetchList.endpointUrl).to(equal(env.apiUrl + endpoint))
+            }
+
+        }
 
     }
-
-  }
 }
 
