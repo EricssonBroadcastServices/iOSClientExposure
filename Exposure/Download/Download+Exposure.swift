686b94a6ad0409d7f6693ad22aadca0f58d7f205
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-10-13 11:27:58 +0200

    EMP-10486 Transfered offline asset management from refapp.

--- Exposure/Download/Download+Exposure.swift
+++ Exposure/Download/Download+Exposure.swift
@@ -11,144 +11,28 @@ import AVFoundation
 import Download
 import Player
 
-/// *Exposure* specific implementation of the `OfflineFairplayRequester` protocol.
-///
-/// This class handles any *Exposure* related `DRM` validation with regards to *Fairplay*. It is designed to be *plug-and-play* and should require no configuration to use.
-internal class ExposureDownloadFairplayRequester: NSObject, ExposureFairplayRequester, DownloadFairplayRequester, FairplayRequester {
-    
-    init(entitlement: PlaybackEntitlement, assetId: String) {
-        self.entitlement = entitlement
-        self.assetId = assetId
-    }
-    
-//    4.4.4.5.  EXT-X-SESSION-KEY
-//    The EXT-X-SESSION-KEY tag allows encryption keys from Media Playlists
-//    to be specified in a Master Playlist.  This allows the client to
-//    preload these keys without having to read the Media Playlist(s)
-//    first.
-//    Its format is:
-//    #EXT-X-SESSION-KEY:<attribute-list>
-//    All attributes defined for the EXT-X-KEY tag (Section 4.4.2.4) are
-//    also defined for the EXT-X-SESSION-KEY, except that the value of the
-//    METHOD attribute MUST NOT be NONE.  If an EXT-X-SESSION-KEY is used,
-//    the values of the METHOD, KEYFORMAT and KEYFORMATVERSIONS attributes
-//    MUST match any EXT-X-KEY with the same URI value.
-//    EXT-X-SESSION-KEY tags SHOULD be added if multiple Variant Streams or
-//    Renditions use the same encryption keys and formats.  A EXT-X
-//    -SESSION-KEY tag is not associated with any particular Media
-//    Playlist.
-//    A Master Playlist MUST NOT contain more than one EXT-X-SESSION-KEY
-//    tag with the same METHOD, URI, IV, KEYFORMAT, and KEYFORMATVERSIONS
-//    attribute values.
-//    The EXT-X-SESSION-KEY tag is optional.
-    
-    internal let assetId: String
-    internal let entitlement: PlaybackEntitlement
-    internal let resourceLoadingRequestQueue = DispatchQueue(label: "com.emp.exposure.offline.fairplay.requests")
-    internal let customScheme = "skd"
-    internal let resourceLoadingRequestOptions: [String : AnyObject]? = [AVAssetResourceLoadingRequestStreamingContentKeyRequestRequiresPersistentKey: true as AnyObject]
-    
-    
-    internal func onSuccessfulRetrieval(of ckc: Data, for resourceLoadingRequest: AVAssetResourceLoadingRequest) throws -> Data {
-        // Obtain a persistable content key from a context.
-        //
-        // The data returned from this method may be used to immediately satisfy an
-        // AVAssetResourceLoadingDataRequest, as well as any subsequent requests for the same key url.
-        //
-        // The value of AVAssetResourceLoadingContentInformationRequest.contentType must be set to AVStreamingKeyDeliveryPersistentContentKeyType when responding with data created with this method.
-        var error: NSError?
-        let persistedCKC = resourceLoadingRequest.persistentContentKey(fromKeyVendorResponse: ckc, options: nil, error: &error)
-        
-        guard error == nil else {
-            print("persistentContentKey",error!.localizedDescription)
-            throw error!
-        }
-        
-        let persistedKeyURL = try contentKeyUrl(for: assetId)
-        print("onSuccessfulRetrieval CKC",assetId,persistedKeyURL)
-        try persistedCKC.write(to: persistedKeyURL)
-        
-        return persistedCKC
-    }
-    
-    func shouldContactRemote(for resourceLoadingRequest: AVAssetResourceLoadingRequest) throws -> Bool {
-        guard resourceLoadingRequest.contentInformationRequest != nil else {
-            throw ExposureError.fairplay(reason: .contentInformationRequestMissing)
-        }
-        
-        resourceLoadingRequest.contentInformationRequest?.contentType = AVStreamingKeyDeliveryPersistentContentKeyType
-        
-        guard let dataRequest = resourceLoadingRequest.dataRequest else {
-            throw ExposureError.fairplay(reason: .missingDataRequest)
-        }
-        
-        // Check if we can handle the request with a previously persisted content key
-        if let keyData = try persistedContentKey(for: assetId) {
-            dataRequest.respond(with: keyData)
-            resourceLoadingRequest.finishLoading()
-            return false
-        }
-        return true
-    }
-}
-
-// FIX ALL THIS PERSISTING + LOADING PERSISTED KEYS.
-// FIX Offline vs Download Fairplay requester
-extension ExposureDownloadFairplayRequester {
-    internal func persistedContentKey(for assetId: String) throws -> Data? {
-        let url = try contentKeyUrl(for: assetId)
-        print("Persisted CKC",assetId,url)
-        guard FileManager.default.fileExists(atPath: url.path) else {
-            print("Persisted CKC? : NO")
-            return nil
-        }
-        print("Persisted CKC? : YES")
-        return try Data(contentsOf: url)
-    }
-}
-extension ExposureDownloadFairplayRequester {
-    internal func contentKeyDirectory() throws -> URL {
-        return try FileManager
-            .default
-            .url(for: .documentDirectory,
-                 in: .userDomainMask,
-                 appropriateFor: nil,
-                 create: true)
-            .appendingPathComponent("emp")
-            .appendingPathComponent("exposure")
-            .appendingPathComponent("offlineMedia")
-            .appendingPathComponent("keys", isDirectory: true)
-    }
-    
-    internal func contentKeyUrl(for assetId: String) throws -> URL {
-        return try contentKeyDirectory().appendingPathComponent("\(assetId)-key")
-    }
-}
-
-// MARK: - AVAssetResourceLoaderDelegate
-extension ExposureDownloadFairplayRequester {
-    public func resourceLoader(_ resourceLoader: AVAssetResourceLoader, shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest) -> Bool {
-        return canHandle(resourceLoadingRequest: loadingRequest)
-    }
-    
-    public func resourceLoader(_ resourceLoader: AVAssetResourceLoader, shouldWaitForRenewalOfRequestedResource renewalRequest: AVAssetResourceRenewalRequest) -> Bool {
-        return canHandle(resourceLoadingRequest: renewalRequest)
-    }
-}
-
-extension Downloader {
+extension Download.SessionManager {
     /// Create a `DownloadTask` for an asset as specified by a `PlaybackEntitlement` supplied through exposure.
     ///
     /// If the requested content is *FairPlay* protected, the appropriate `OfflineFairplayRequester` will be created. Configuration will be taken from the supplied `entitlement`.
     ///
     /// - parameter entitlement: *Exposure* provided entitlement
     /// - parameter assetId: A unique identifier for the entitlement. This is most likely *EMP asset id*
+    /// - throws: `DownloadError`
     @available(iOS 10.0, *)
-    public static func download(entitlement: PlaybackEntitlement, assetId: String) throws -> DownloadTask {
+    public func download(entitlement: PlaybackEntitlement, assetId: String) throws -> DownloadTask {
         let fairplayRequester = ExposureDownloadFairplayRequester(entitlement: entitlement, assetId: assetId)
         
-        // TODO: Download name should probably be retrieved from *Exposure*
-        return try download(mediaLocator: entitlement.mediaLocator, assetId: assetId, using: fairplayRequester, artwork: nil)
+        guard let url = URL(string: entitlement.mediaLocator) else {
+            // TODO: Init DownloadTask with error instead of throwing
+            throw DownloadError.invalidMediaUrl(path: entitlement.mediaLocator)
+        }
+        
+        // Store an initial locator to indicate download is underway
+        save(assetId: assetId, entitlement: entitlement, url: nil)
+        
+        // TODO: Artwork should probably be retrieved from *Exposure*
+        return download(mediaLocator: url, assetId: assetId, artwork: nil, using: fairplayRequester)
     }
     
     /// Create a `DownloadTask` for an asset as specified by a `PlaybackEntitlement` supplied through exposure.
@@ -158,11 +42,178 @@ extension Downloader {
     /// - parameter entitlement: *Exposure* provided entitlement
     /// - parameter assetId: A unique identifier for the entitlement. This is most likely *EMP asset id*
     /// - parameter url: Destination URL for the download.
+    /// - throws: `DownloadError`
     @available(iOS, introduced: 9.0, deprecated: 10.0)
-    public static func download(entitlement: PlaybackEntitlement, assetId: String, to url: URL) throws -> DownloadTask {
+    public func download(entitlement: PlaybackEntitlement, assetId: String, to url: URL) throws -> DownloadTask {
         let fairplayRequester = ExposureDownloadFairplayRequester(entitlement: entitlement, assetId: assetId)
         
-        return try download(mediaLocator: entitlement.mediaLocator, assetId: assetId, to: url, using: fairplayRequester)
+        guard let url = URL(string: entitlement.mediaLocator) else {
+            // TODO: Init DownloadTask with error instead of throwing
+            throw DownloadError.invalidMediaUrl(path: entitlement.mediaLocator)
+        }
+        // Store an initial locator to indicate download is underway
+        save(assetId: assetId, entitlement: entitlement, url: nil)
+        
+        return download(mediaLocator: url, assetId: assetId, to: url, using: fairplayRequester)
+    }
+}
+
+extension SessionManager {
+    @available(iOS 10.0, *)
+    public func download(assetId: String, callback: @escaping (PlaybackEntitlement) -> Void) {
+        
+    }
+    
+//    @available(iOS, introduced: 9.0, deprecated: 10.0)
+}
+
+// MARK: - OfflineMediaAsset
+extension SessionManager {
+    public func offline(assetId: String) -> OfflineMediaAsset? {
+        return offlineAssets()
+            .filter{ $0.assetId == assetId }
+            .first
+    }
+    
+    public func offlineAssets() -> [OfflineMediaAsset] {
+        guard let localMedia = localMediaRecords else { return [] }
+        return localMedia.map{ resolve(mediaRecord: $0) }
+    }
+    
+    public func delete(media: OfflineMediaAsset) {
+        remove(localRecordId: media.assetId)
+        do {
+            try media.fairplayRequester.deletePersistedContentKey(for: media.assetId)
+            if let url = media.urlAsset?.url {
+                try FileManager.default.removeItem(at: url)
+            }
+            print("✅ SessionManager+Exposure. Cleaned up local media")
+        }
+        catch {
+            print("🚨 SessionManager+Exposure. Failed to clean local media: ",error.localizedDescription)
+        }
+    }
+    
+    public func delete(assetId: String) {
+        guard let media = offline(assetId: assetId) else { return }
+        delete(media: media)
+    }
+    
+    internal func save(assetId: String, entitlement: PlaybackEntitlement, url: URL?) {
+        do {
+            if let currentAsset = offline(assetId: assetId) {
+                print("⚠️ There is another record for an offline asset with id: \(assetId). This data will be overwritten. The location of any downloaded media or content keys will be lost!")
+            }
+            
+            let record = try LocalMediaRecord(assetId: assetId, entitlement: entitlement, completedAt: url)
+            save(localRecord: record)
+        }
+        catch {
+            print("🚨 Unable to bookmark local media record \(assetId): ",error.localizedDescription)
+        }
+    }
+}
+
+// MARK: - LocalMediaRecord
+extension SessionManager {
+    fileprivate var localMediaRecords: [LocalMediaRecord]? {
+        do {
+            let logFile = try logFileURL()
+            
+            if !FileManager.default.fileExists(atPath: logFile.path) {
+                return []
+            }
+            let data = try Data(contentsOf: logFile)
+            
+            let localMedia = try JSONDecoder().decode([LocalMediaRecord].self, from: data)
+            
+            localMedia.forEach{ print("📎 Local media id: \($0.assetId)") }
+            return localMedia
+        }
+        catch {
+            print("localMediaLog failed",error.localizedDescription)
+            return nil
+        }
+    }
+    
+    fileprivate func resolve(mediaRecord: LocalMediaRecord) -> OfflineMediaAsset {
+        var bookmarkDataIsStale = false
+        guard let urlBookmark = mediaRecord.urlBookmark else {
+            return OfflineMediaAsset(assetId: mediaRecord.assetId, entitlement: mediaRecord.entitlement, url: nil)
+        }
+        
+        do {
+            guard let url = try URL(resolvingBookmarkData: urlBookmark, bookmarkDataIsStale: &bookmarkDataIsStale) else {
+                return OfflineMediaAsset(assetId: mediaRecord.assetId, entitlement: mediaRecord.entitlement, url: nil)
+                
+            }
+            
+            guard !bookmarkDataIsStale else {
+                return OfflineMediaAsset(assetId: mediaRecord.assetId, entitlement: mediaRecord.entitlement, url: nil)
+            }
+            
+            return OfflineMediaAsset(assetId: mediaRecord.assetId, entitlement: mediaRecord.entitlement, url: url)
+        }
+        catch {
+            return OfflineMediaAsset(assetId: mediaRecord.assetId, entitlement: mediaRecord.entitlement, url: nil)
+        }
+    }
+}
+
+// MARK: Directory
+extension SessionManager {
+    fileprivate var localMediaRecordsFile: String {
+        return "localMediaRecords"
+    }
+    
+    fileprivate func logFileURL() throws -> URL {
+        return try baseDirectory().appendingPathComponent(localMediaRecordsFile)
+    }
+    
+    /// This directory should be reserved for analytics data.
+    ///
+    /// - returns: `URL` to the base directory
+    /// - throws: `FileManager` error
+    fileprivate func baseDirectory() throws -> URL {
+        return try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("emp")
+            .appendingPathComponent("exposure")
+            .appendingPathComponent("offlineMedia")
+    }
+}
+
+// MARK: Save / Remove
+extension SessionManager {
+    
+    /// This method will ensure `LocalMediaLog` has a unique list of downloads with respect to `assetId`
+    fileprivate func save(localRecord: LocalMediaRecord) {
+        let localMedia = localMediaRecords ?? []
+        
+        var filteredLog = localMedia.filter{ $0.assetId != localRecord.assetId }
+        
+        filteredLog.append(localRecord)
+        save(mediaLog: filteredLog)
+        print("✅ Saved bookmark for local media record \(localRecord.assetId): ")
+    }
+    
+    fileprivate func save(mediaLog: [LocalMediaRecord]) {
+        do {
+            let logURL = try baseDirectory()
+            
+            let data = try JSONEncoder().encode(mediaLog)
+            try data.persist(as: localMediaRecordsFile, at: logURL)
+        }
+        catch {
+            print("save(mediaLog:) failed",error.localizedDescription)
+        }
+    }
+    
+    internal func remove(localRecordId: String) {
+        guard let localMedia = localMediaRecords else { return }
+        
+        /// Update and save new log
+        let newLog = localMedia.filter{ $0.assetId != localRecordId }
+        save(mediaLog: newLog)
     }
 }
 
---
+++ Exposure/Download/OfflineMediaAsset.swift
@@ -0,0 +1,158 @@
+//
+//  OfflineMediaAsset.swift
+//  iOSReferenceApp
+//
+//  Created by Fredrik Sjöberg on 2017-10-06.
+//  Copyright © 2017 emp. All rights reserved.
+//
+
+import Foundation
+import Download
+import AVFoundation
+
+public struct OfflineMediaAsset {
+    internal init(assetId: String, entitlement: PlaybackEntitlement, url: URL?) {
+        self.assetId = assetId
+        self.entitlement = entitlement
+        self.fairplayRequester = ExposureDownloadFairplayRequester(entitlement: entitlement, assetId: assetId)
+        if let url = url {
+            self.urlAsset = AVURLAsset(url: url)
+        }
+        else {
+            self.urlAsset = nil
+        }
+    }
+    
+    public let assetId: String
+    public let entitlement: PlaybackEntitlement
+    internal let urlAsset: AVURLAsset?
+    internal let fairplayRequester: ExposureDownloadFairplayRequester
+    
+    public func state(callback: @escaping (State) -> Void) {
+        guard let urlAsset = urlAsset else {
+            callback(.notPlayable)
+            return
+        }
+        
+        if #available(iOS 10.0, *) {
+            print("PlayableOffline: ",urlAsset.url,urlAsset.assetCache?.isPlayableOffline)
+            if let assetCache = urlAsset.assetCache, assetCache.isPlayableOffline {
+                callback(.completed)
+                return
+            }
+        }
+        
+        urlAsset.loadValuesAsynchronously(forKeys: ["playable"]) {
+            DispatchQueue.main.async {
+                
+                // Check for any issues preparing the loaded values
+                var error: NSError?
+                if urlAsset.statusOfValue(forKey: "playable", error: &error) == .loaded {
+                    if urlAsset.isPlayable {
+                        callback(.completed)
+                    }
+                    else {
+                        callback(.notPlayable)
+                    }
+                }
+                else {
+                    callback(.notPlayable)
+                }
+            }
+        }
+    }
+    
+    public enum State {
+        case completed
+        case notPlayable
+    }
+}
+
+
+internal struct LocalMediaRecord: Codable {
+    /// URL encoded as bookmark data
+    internal var urlBookmark: Data? {
+        switch downloadState {
+        case .completed(urlBookmark: let data): return data
+        case .inProgress: return nil
+        }
+    }
+    
+    /// State
+    internal let downloadState: DownloadState
+    
+    /// Id for the asset at `bookmarkURL`
+    internal let assetId: String
+    
+    /// Related entitlement
+    internal let entitlement: PlaybackEntitlement
+    
+    internal init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        assetId = try container.decode(String.self, forKey: .assetId)
+        entitlement = try container.decode(PlaybackEntitlement.self, forKey: .entitlement)
+        
+        if let data = try container.decodeIfPresent(Data.self, forKey: .downloadState) {
+            downloadState = .completed(urlBookmark: data)
+        }
+        else {
+            downloadState = .inProgress
+        }
+    }
+    
+    internal func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(assetId, forKey: .assetId)
+        try container.encode(entitlement, forKey: .entitlement)
+        
+        switch downloadState {
+        case .completed(urlBookmark: let data): try container.encode(data, forKey: .downloadState)
+        default: return
+        }
+    }
+    
+    internal init(assetId: String, entitlement: PlaybackEntitlement, completedAt location: URL?) throws {
+        self.assetId = assetId
+        self.entitlement = entitlement
+        
+        if let data = try location?.bookmarkData() {
+            downloadState = .completed(urlBookmark: data)
+        }
+        else {
+            downloadState = .inProgress
+        }
+    }
+    
+    internal enum DownloadState {
+        
+        /// URL encoded as bookmark data
+        case completed(urlBookmark: Data)
+        
+        /// No destination might have been set
+        case inProgress
+    }
+    
+    internal enum CodingKeys: String, CodingKey {
+        case downloadState
+        case assetId
+        case entitlement
+    }
+}
+
+
+extension Data {
+    /// Convenience function for persisting a `Data` blob through `FileManager`.
+    ///
+    /// - parameter filename: Name of the file, including extension
+    /// - parameter directoryUrl: `URL` to the storage directory
+    /// - throws: `FileManager` related `Error` or `Data` related error in the *Cocoa Domain*
+    internal func persist(as filename: String, at directoryUrl: URL) throws {
+        if !FileManager.default.fileExists(atPath: directoryUrl.path) {
+            try FileManager.default.createDirectory(at: directoryUrl, withIntermediateDirectories: true, attributes: nil)
+        }
+        
+        try self.write(to: directoryUrl.appendingPathComponent(filename))
+    }
+}
