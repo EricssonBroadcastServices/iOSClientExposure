b720bb446527f5b2eb4a8fbe531af901614ce626
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2018-01-08 19:24:52 +0100

    Added Monotonic Time service

--- Exposure/Player/ExposureContext.swift
+++ Exposure/Player/ExposureContext.swift
@@ -9,6 +9,135 @@
 import Foundation
 import Player
 
+
+public class MonotonicTimeService {
+    internal let refreshInterval: Int
+    internal let exposureDownInterval: Int
+    
+    
+    public init(environment: Environment, refreshInterval: Int = 60000 * 30, errorRetryInterval: Int = 1000) {
+        self.environment = environment
+        self.refreshInterval = refreshInterval
+        self.exposureDownInterval = errorRetryInterval
+        
+        let queue = DispatchQueue(label: "com.emp.exposure.monotonicTimeService",
+                                  qos: DispatchQoS.background,
+                                  attributes: DispatchQueue.Attributes.concurrent)
+        self.queue = queue
+        timer = DispatchSource.makeTimerSource(queue: queue)
+        
+    }
+    
+    deinit {
+        timer.setEventHandler{}
+        timer.cancel()
+    }
+    
+    private var environment: Environment
+    private let queue: DispatchQueue
+    private let timer: DispatchSourceTimer
+    
+    
+    private var state: State = .notStarted
+    private enum State {
+        case notStarted
+        case running(latest: Difference?)
+        case retrying(previous: Difference?)
+        
+        var currentDifference: Difference? {
+            switch self {
+            case .notStarted: return nil
+            case .running(latest: let latest): return latest
+            case .retrying(previous: let previous): return previous
+            }
+        }
+    }
+    
+    internal struct Difference {
+        internal var serverStartTime: Int64
+        internal var localStartTime: Int64
+        
+        var monotonicTime: Int64 {
+            return serverStartTime + Date().millisecondsSince1970 - localStartTime
+        }
+    }
+    
+    public var currentTime: Int64? {
+        switch state {
+        case .notStarted:
+            startTimer{ _ in }
+            return nil
+        case .running(latest: let difference):
+            return difference?.monotonicTime
+        case .retrying(previous: let difference):
+            return difference?.monotonicTime
+        }
+    }
+    
+    public func currentTime(forceRefresh: Bool = true, callback: @escaping (Int64?) -> Void) {
+        switch state {
+        case .notStarted:
+            startTimer(callback: callback)
+        case .running(latest: let latest):
+            if forceRefresh {
+                fetchServerTime(callback: callback)
+            }
+            else {
+                callback(latest?.monotonicTime)
+            }
+        case .retrying(previous: let previous):
+            if forceRefresh {
+                fetchServerTime(callback: callback)
+            }
+            else {
+                callback(previous?.monotonicTime)
+            }
+        }
+    }
+    
+    private func startTimer(callback: @escaping (Int64?) -> Void) {
+        timer.scheduleOneshot(deadline: .now(), leeway: .seconds(1))
+        
+        timer.setEventHandler{ [weak self] in
+            guard let `self` = self else { return }
+            self.fetchServerTime(callback: callback)
+        }
+        
+        state = .running(latest: nil)
+        timer.resume()
+    }
+    
+    private func fetchServerTime(callback: @escaping (Int64?) -> Void) {
+        FetchServerTime(environment: self.environment)
+            .request()
+            .response{ [weak self] in
+                guard let `self` = self else { return }
+                if let value = $0.value?.epochMillis {
+                    self.queue.sync {
+                        self.state = .running(latest: Difference(serverStartTime: Int64(value), localStartTime: Date().millisecondsSince1970))
+                        self.timer.scheduleOneshot(deadline: .now() + .milliseconds(self.refreshInterval), leeway: .seconds(1))
+                        callback(self.state.currentDifference?.monotonicTime)
+                        print("Fired Refresh")
+                    }
+                }
+                else if $0.error != nil {
+                    self.queue.sync {
+                        self.state = .retrying(previous: self.state.currentDifference)
+                        self.timer.scheduleOneshot(deadline: .now() + .milliseconds(self.exposureDownInterval), leeway: .seconds(1))
+                        print("Error during refresh. Rescheduling")
+                        callback(self.state.currentDifference?.monotonicTime)
+                    }
+                }
+        }
+    }
+}
+
+//internal protocol MonotonicTimeNetworkHandler {
+//    func fetchServerTime(using environment: Environment, callback: @escaping (ServerTime?, ExposureError?) -> Void)
+//}
+
+
+
 /// Defines the `MediaContext` to be used when contacting *Exposure*.
 ///
 /// Allows retrieval and processing of `PlaybackEntitlement`s through designated extensions on `Player`.
@@ -27,6 +156,9 @@ public class ExposureContext: MediaContext {
     
     /// Token identifying the active session.
     public let sessionToken: SessionToken
+    
+    ///
+    
     
     public init(environment: Environment, sessionToken: SessionToken) {
         self.environment = environment
---
+++ ExposureTests/MonotonicTimeServiceSpec.swift
@@ -0,0 +1,23 @@
+//
+//  MonotonicTimeServiceSpec.swift
+//  ExposureTests
+//
+//  Created by Fredrik Sjöberg on 2018-01-08.
+//  Copyright © 2018 emp. All rights reserved.
+//
+
+import Foundation
+
+@testable import Exposure
+
+class MonotonicTimeServiceSpec: QuickSpec {
+    override func spec() {
+        super.spec()
+        
+        describe("MonotonicTimeService") {
+            it("should ") {
+                
+            }
+        }
+    }
+}
