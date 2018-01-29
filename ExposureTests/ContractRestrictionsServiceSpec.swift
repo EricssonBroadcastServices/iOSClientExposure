f78b388252c6708af9ce14fc214601a10caee7cb
Fredrik Sjöberg <fredrik.sjoberg@webstep.se>
2018-01-29 15:29:37 +0100

    Added missing unit tests

--- ExposureTests/ContractRestrictionsServiceSpec.swift
+++ ExposureTests/ContractRestrictionsServiceSpec.swift
@@ -12,21 +12,95 @@ import Foundation
 
 @testable import Exposure
 
-class ContractRestrictionsServiceSpec: ProgramProvider {
+class ContractRestrictionsServiceSpec: QuickSpec {
     override func spec() {
         super.spec()
         
         describe("ContractRestrictionsService") {
             let service = ContractRestrictionsService()
-            let entitmelent 
-            it("should convey restrictions from entitlement") {
-                let noneEnabled = self.buildEntitlement(ffEnabled: false, rwEnabled: false, timeshiftEnabled: false)
-                
-                let ff = service.canSeek(from: 0, to: 10, using: noneEnabled)
-                let rw = service.canSeek(from: 10, to: 0, using: noneEnabled)
-                let pause = service.canPause(entitlement: noneEnabled)
-                
-                expect(ff).to
+            
+            context("All disabled") {
+                it("should convey restrictions from entitlement") {
+                    let noneEnabled = self.buildEntitlement(ffEnabled: false, rwEnabled: false, timeshiftEnabled: false)
+                    
+                    let ff = service.canSeek(from: 0, to: 10, using: noneEnabled)
+                    let rw = service.canSeek(from: 10, to: 0, using: noneEnabled)
+                    let pause = service.canPause(entitlement: noneEnabled)
+                    
+                    expect(ff).to(beFalse())
+                    expect(rw).to(beFalse())
+                    expect(pause).to(beFalse())
+                }
+            }
+            
+            context("ff enabled") {
+                it("should convey restrictions from entitlement") {
+                    let noneEnabled = self.buildEntitlement(ffEnabled: true, rwEnabled: false, timeshiftEnabled: false)
+                    
+                    let ff = service.canSeek(from: 0, to: 10, using: noneEnabled)
+                    let rw = service.canSeek(from: 10, to: 0, using: noneEnabled)
+                    let pause = service.canPause(entitlement: noneEnabled)
+                    
+                    expect(ff).to(beTrue())
+                    expect(rw).to(beFalse())
+                    expect(pause).to(beFalse())
+                }
+            }
+            
+            context("rw enabled") {
+                it("should convey restrictions from entitlement") {
+                    let noneEnabled = self.buildEntitlement(ffEnabled: false, rwEnabled: true, timeshiftEnabled: false)
+                    
+                    let ff = service.canSeek(from: 0, to: 10, using: noneEnabled)
+                    let rw = service.canSeek(from: 10, to: 0, using: noneEnabled)
+                    let pause = service.canPause(entitlement: noneEnabled)
+                    
+                    expect(ff).to(beFalse())
+                    expect(rw).to(beTrue())
+                    expect(pause).to(beFalse())
+                }
+            }
+            
+            context("pause enabled") {
+                it("should convey restrictions from entitlement") {
+                    let noneEnabled = self.buildEntitlement(ffEnabled: false, rwEnabled: false, timeshiftEnabled: true)
+                    
+                    let ff = service.canSeek(from: 0, to: 10, using: noneEnabled)
+                    let rw = service.canSeek(from: 10, to: 0, using: noneEnabled)
+                    let pause = service.canPause(entitlement: noneEnabled)
+                    
+                    expect(ff).to(beFalse())
+                    expect(rw).to(beFalse())
+                    expect(pause).to(beTrue())
+                }
+            }
+            
+            context("ff and rw enabled") {
+                it("should convey restrictions from entitlement") {
+                    let noneEnabled = self.buildEntitlement(ffEnabled: true, rwEnabled: true, timeshiftEnabled: false)
+                    
+                    let ff = service.canSeek(from: 0, to: 10, using: noneEnabled)
+                    let rw = service.canSeek(from: 10, to: 0, using: noneEnabled)
+                    let pause = service.canPause(entitlement: noneEnabled)
+                    
+                    expect(ff).to(beTrue())
+                    expect(rw).to(beTrue())
+                    expect(pause).to(beFalse())
+                }
+            }
+            
+            context("all enabled") {
+                it("should convey restrictions from entitlement") {
+                    let noneEnabled = self.buildEntitlement(ffEnabled: true, rwEnabled: true, timeshiftEnabled: true)
+                    
+                    let ff = service.canSeek(from: 0, to: 10, using: noneEnabled)
+                    let rw = service.canSeek(from: 10, to: 0, using: noneEnabled)
+                    let pause = service.canPause(entitlement: noneEnabled)
+                    
+                    expect(ff).to(beTrue())
+                    expect(rw).to(beTrue())
+                    expect(pause).to(beTrue())
+                }
             }
         }
     }
