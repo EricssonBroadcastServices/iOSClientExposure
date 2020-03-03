b993f4d3f6f7fc4e4b9c56b8350222fabde01e0e
Johnny Sundblom <johnny.sundblom@gmail.com>
2020-03-03 10:28:13 +0100

    Added lastViewedOffset to ContinueTVShow class.

--- Exposure/UserPlayHistory/Response/ContinueTVShow.swift
+++ Exposure/UserPlayHistory/Response/ContinueTVShow.swift
@@ -10,18 +10,24 @@ import Foundation
 
 /// `ContinueTVShow` contains *ContinueTVShow* information about the new episode to play for a tv-series.
 public struct ContinueTVShow: Decodable {
+    /// The next asset/episode to play
     public let asset: Asset
+    /// Should start watching or continue tv-show
     public let startedWatching: Bool
+    /// Last viewed offset, offset in the last play of the asset.
+    public let lastViewedOffset: Int
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
         asset = try container.decode(Asset.self, forKey: .asset)
         startedWatching = try container.decode(Bool.self, forKey: .startedWatching)
+        lastViewedOffset = try container.decode(Int.self, forKey: .lastViewedOffset)
     }
     
     internal enum CodingKeys: String, CodingKey {
         case asset
         case startedWatching
-    }
+        case lastViewedOffset
+    } 
 }
