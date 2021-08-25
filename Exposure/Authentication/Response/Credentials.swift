0fc081c4b06bdd203d8e967984a9a8c741be3ef3
delarge77 <lastripper>
2021-08-25 15:40:15 +0200

    - Store display name, email and user name

--- Exposure/Authentication/Response/Credentials.swift
+++ Exposure/Authentication/Response/Credentials.swift
@@ -8,11 +8,34 @@
 
 import Foundation
 
+public struct UserProfile: Decodable {
+    public let username: String?
+    public let displayName: String?
+    public let emailAddress: String?
+    
+    public init(username: String?, displayName: String?, emailAddress: String?) {
+        self.username = username
+        self.displayName = displayName
+        self.emailAddress = emailAddress
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        username = try container.decodeIfPresent(String.self, forKey: .username)
+        displayName = try container.decodeIfPresent(String.self, forKey: .displayName)
+        emailAddress = try container.decodeIfPresent(String.self, forKey: .emailAddress)
+    }
+    
+    fileprivate enum CodingKeys: String, CodingKey {
+        case username, displayName, emailAddress
+    }
+}
+
 /// `Credentials` response returned on a successful login attempt.
 public struct Credentials: Decodable {
     /// Keys used to specify `json` body for the request.
     fileprivate enum CodingKeys: String, CodingKey {
-        case sessionToken, crmToken, accountId, accountStatus
+        case sessionToken, crmToken, accountId, accountStatus, userProfile
         case expiration = "expirationDateTime"
     }
     
@@ -31,12 +54,15 @@ public struct Credentials: Decodable {
     /// The status of the account
     public let accountStatus: String?
     
-    public init(sessionToken: SessionToken, crmToken: String?, accountId: String?, expiration: Date?, accountStatus: String?) {
+    public let userProfile: UserProfile?
+    
+    public init(sessionToken: SessionToken, crmToken: String?, accountId: String?, expiration: Date?, accountStatus: String?, userProfile: UserProfile?) {
         self.sessionToken = sessionToken
         self.crmToken = crmToken
         self.accountId = accountId
         self.expiration = expiration
         self.accountStatus = accountStatus
+        self.userProfile = userProfile
     }
 
     public init(from decoder: Decoder) throws {
@@ -45,6 +71,7 @@ public struct Credentials: Decodable {
         crmToken = try container.decodeIfPresent(String.self, forKey: .crmToken)
         accountId = try container.decodeIfPresent(String.self, forKey: .accountId)
         accountStatus = try container.decodeIfPresent(String.self, forKey: .accountStatus)
+        userProfile = try container.decodeIfPresent(UserProfile.self, forKey: .userProfile)
         guard let dataString = try container.decodeIfPresent(String.self, forKey: .expiration) else {
             expiration = nil
             return
