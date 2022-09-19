02df4a9d4e7b2a760222fc4dcb3b8944e7b56120
udayasri <udayasricl@gmail.com>
2022-09-19 15:26:12 +0200

    Add global variable to keep track of the framework version

---
+++ Sources/iOSClientExposure/Version.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  iOSClientExposure
+//
+//  Created by Udaya Sri Senarathne on 2022-09-19.
+//
+
+import Foundation
+
+/// Global constant for framework version
+public let ExposureVersion = "3.1.100"
