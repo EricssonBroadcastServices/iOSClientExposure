e4a7ee8605fd6fd3c6a1934f87ac96a7dfa3b81c
udayasri <udayasricl@gmail.com>
2023-05-15 09:43:12 +0200

    Remove unused objc files

--- Sources/iOSClientExposureObjc/Dependencies/Crypto/Crypto.h
+++ Sources/iOSClientExposureObjc/Dependencies/Crypto/Crypto.h
@@ -1,31 +1,31 @@
+////
+////  Crypto.h
+////  Utilities
+////
+////  Created by Fredrik Sjöberg on 2017-07-28.
+////  Copyright © 2017 emp. All rights reserved.
+////
 //
-//  Crypto.h
-//  Utilities
+//#import <Foundation/Foundation.h>
 //
-//  Created by Fredrik Sjöberg on 2017-07-28.
-//  Copyright © 2017 emp. All rights reserved.
+//@interface NSData (Crypto)
 //
-
-#import <Foundation/Foundation.h>
-
-@interface NSData (Crypto)
-
-/// Encrypts `Self`
-///
-/// - parameter key: Raw key material
-/// - parameter iv: Initialization vector
-/// - parameter error: NSError
-/// - returns: `Self` encrypted
-- (NSData *)AES256EncryptedUsingKey:(id)key iv:(NSString *)iv error:(NSError **)error;
-
-/// Decrypts `Self`
-///
-/// - parameter key: Raw key material
-/// - parameter iv: Initialization vector
-/// - parameter error: NSError
-/// - returns: `Self` decrypted
-- (NSData *)AES256DecryptedUsingKey:(id)key iv:(NSString *)iv error:(NSError **)error;
-
-/// Returns `Self` as `sha256` hash
-- (NSData *) SHA256Hash;
-@end
+///// Encrypts `Self`
+/////
+///// - parameter key: Raw key material
+///// - parameter iv: Initialization vector
+///// - parameter error: NSError
+///// - returns: `Self` encrypted
+//- (NSData *)AES256EncryptedUsingKey:(id)key iv:(NSString *)iv error:(NSError **)error;
+//
+///// Decrypts `Self`
+/////
+///// - parameter key: Raw key material
+///// - parameter iv: Initialization vector
+///// - parameter error: NSError
+///// - returns: `Self` decrypted
+//- (NSData *)AES256DecryptedUsingKey:(id)key iv:(NSString *)iv error:(NSError **)error;
+//
+///// Returns `Self` as `sha256` hash
+//- (NSData *) SHA256Hash;
+//@end
