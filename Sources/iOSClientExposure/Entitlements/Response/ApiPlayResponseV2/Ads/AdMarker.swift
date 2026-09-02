22d2a97acb46645056a4d14a601c98b2f8fdf38a
Axel Nowaczyk <axelnowaczyk@gmail.com>
2026-09-02 15:30:58 +0200

    [EMP-22899] add offset in AdMarker

--- Sources/iOSClientExposure/Entitlements/Response/ApiPlayResponseV2/Ads/AdMarker.swift
+++ Sources/iOSClientExposure/Entitlements/Response/ApiPlayResponseV2/Ads/AdMarker.swift
@@ -9,18 +9,21 @@ import Foundation
 
 public struct AdMarker: Codable {
 	public let type: String?
+	public let offset: Int?
 	public let tagType: String?
 	public let adTag: String?
 
 	public init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 		type = try container.decodeIfPresent(String.self, forKey: .type)
+		offset = try container.decodeIfPresent(Int.self, forKey: .offset)
 		tagType = try container.decodeIfPresent(String.self, forKey: .tagType)
 		adTag = try container.decodeIfPresent(String.self, forKey: .adTag)
 	}
 
 	internal enum CodingKeys: String, CodingKey {
 		case type
+		case offset
 		case tagType
 		case adTag
 	}
